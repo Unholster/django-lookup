1a8e99f4352ff583094091b5f2bb8ff4897b6bf9
Sebastián Acuña <sacuna@gmail.com>
2015-09-24 15:55:45 -0300

    PEP-8 and Python3

--- lookup/__init__.py
+++ lookup/__init__.py
@@ -1,13 +1,16 @@
 from django.db import IntegrityError
-from models import Alias, ContentType
+from .models import Alias, ContentType
 from unidecode import unidecode
 from difflib import get_close_matches
 import re
+import collections
 
 STRIP_RE = re.compile(r'[^a-z0-9#]')
 
+
 def default_normalization(text):
     return STRIP_RE.sub('', unidecode(text.lower().replace("y", "i")))
+
 
 class LookupCache(dict):
     def __setitem__(self, key, value):
@@ -15,14 +18,17 @@ class LookupCache(dict):
             raise ValueError('None value not allowed in LookupCache')
         super(LookupCache, self).__setitem__(key, value)
 
+
 class LookupTable(object):
-    def __init__(self, modelclass, key_prefix=None, persist=True, filters=None):
+    def __init__(
+        self, modelclass, key_prefix=None, persist=True, filters=None
+    ):
         self.model = modelclass
         self.key_prefix = key_prefix
-        if type(filters) is dict: 
-            self.filtered =  self.model.objects.filter(**filters)
-        elif type(filters) is list: 
-            self.filtered =  self.model.objects.filter(*filters)
+        if type(filters) is dict:
+            self.filtered = self.model.objects.filter(**filters)
+        elif type(filters) is list:
+            self.filtered = self.model.objects.filter(*filters)
         else:
             self.filtered = self.model.objects.all()
         self.persist = persist
@@ -30,7 +36,7 @@ class LookupTable(object):
         self.refresh_cache()
 
     def __iter__(self):
-        return self._cache.itervalues()
+        return iter(self._cache.values())
 
     def lookup(self, *keys):
         return self._cache[self.normalize(*keys)]
@@ -38,24 +44,24 @@ class LookupTable(object):
     def clean(self):
         if self.persist:
             for alias in Alias.objects.filter(
-                        target_type=ContentType.objects.get_for_model(self.model),
-                    ):
+                target_type=ContentType.objects.get_for_model(self.model),
+            ):
                 if alias.target is None:
                     alias.delete()
             self.refresh_cache()
 
     def refresh_cache(self):
         aliases = Alias.objects.filter(
-                target_type=ContentType.objects.get_for_model(self.model),
-                target_id__in=self.filtered
-            )
-        self._cache = LookupCache({ a.key: a.target for a in aliases })
+            target_type=ContentType.objects.get_for_model(self.model),
+            target_id__in=self.filtered
+        )
+        self._cache = LookupCache({a.key: a.target for a in aliases})
 
     def delete_aliases(self):
         aliases = Alias.objects.filter(
-                target_type=ContentType.objects.get_for_model(self.model),
-                target_id__in=self.filtered
-            )
+            target_type=ContentType.objects.get_for_model(self.model),
+            target_id__in=self.filtered
+        )
         aliases.delete()
 
     def normalize(self, *keys):
@@ -77,23 +83,25 @@ class LookupTable(object):
         model_label = self.model.__name__.title()
 
         while not obj:
-            print "%s not found for %s <%s>" % (model_label, text, self.normalize(text))
-            print "Options:"
-            print "  0 - Ignore"
+            print("%s not found for %s <%s>" % (
+                model_label, text, self.normalize(text)
+            ))
+            print("Options:")
+            print("  0 - Ignore")
             offset = 1
-            suggestions = self.suggestions(text, **kwargs).items()
+            suggestions = list(self.suggestions(text, **kwargs).items())
             for i, (key, suggestion) in enumerate(suggestions):
-                print "  %d - <%s> %s" % (i+offset, key, suggestion)
+                print("  %d - <%s> %s" % (i + offset, key, suggestion))
             if create:
-                print "  c - Create"
-                if callable(defaults):
+                print("  c - Create")
+                if isinstance(defaults, collections.Callable):
                     defaults = defaults(text)
                 create_obj = self.prompt_create(defaults)
 
             try:
-                choice = raw_input("Enter choice:")
+                choice = input("Enter choice:")
             except ValueError:
-                print "Unknown option: %d" % choice
+                print("Unknown option: %d" % choice)
                 continue
 
             if choice == '0':
@@ -104,26 +112,26 @@ class LookupTable(object):
                 created = True
             else:
                 try:
-                    key, obj = suggestions[int(choice)-offset]
-                except ValueError, IndexError:
-                    print "Unknown option: %s" % choice
-        
+                    key, obj = suggestions[int(choice) - offset]
+                except (ValueError, IndexError):
+                    print("Unknown option: %s" % choice)
+
         self.add_alias(text, obj)
         return obj, created
 
     def prompt_create(self, defaults):
         obj = self.model(**defaults)
-        print "******** Create *********"
+        print("******** Create *********")
         for f in self.model._meta.get_all_field_names():
             if hasattr(obj, f):
-                print "   %s: %s" % (f, getattr(obj, f))
-        print "*************************"
+                print("   %s: %s" % (f, getattr(obj, f)))
+        print("*************************")
         return obj
 
     def create_aliases(self, key_gen=None):
         if key_gen is None and hasattr(self.model, 'keygen'):
             key_gen = self.model.keygen
-        elif not callable(key_gen):
+        elif not isinstance(key_gen, collections.Callable):
             field_name = key_gen
             key_gen = lambda o: getattr(o, field_name)
 
@@ -137,12 +145,14 @@ class LookupTable(object):
         key = self.normalize(text)
         if self.persist:
             Alias.objects.create(
-                    key=key,
-                    target=target
-                )
+                key=key,
+                target=target
+            )
         self._cache[key] = target
 
     def suggestions(self, text, n=3, cutoff=0.5):
         normalized = self.normalize(text)
-        matches = get_close_matches(normalized, self._cache.keys(), n=n, cutoff=cutoff)
-        return { k: self._cache[k] for k in matches }
+        matches = get_close_matches(
+            normalized, list(self._cache.keys()), n=n, cutoff=cutoff
+        )
+        return {k: self._cache[k] for k in matches}
